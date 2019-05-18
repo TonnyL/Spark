9f5195451df6086293173e261ffe8dd45d20474e
TonnyL <lizhaotailang@gmail.com>
2019-05-18 23:57:20 +0800

    🚚 chore(dependency): migrate to androidx

--- sample/src/main/java/io/github/tonnyl/sparksample/MainActivity.kt
+++ sample/src/main/java/io/github/tonnyl/sparksample/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.tonnyl.sparksample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import io.github.tonnyl.spark.Spark
 import kotlinx.android.synthetic.main.activity_main.*
 
